0b1a3b8221ba79f3da77ddcf02fbbf415d122920
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-08-13 21:01:29 +0300

    lab13-14

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -4,6 +4,8 @@ import './BestBooks.css';
 import axios from 'axios';
 import { withAuth0 } from "@auth0/auth0-react";
 import Book from './screens/Book'
+import Bookmodel from './compnents/Bookmodel'
+import Button from 'react-bootstrap/Button';
 
 
 export class BestBooks extends React.Component {
@@ -17,26 +19,99 @@ export class BestBooks extends React.Component {
   }
 
   componentDidMount = async () => {
-      await axios.get(`${process.env.REACT_APP_URL}/books?email=${this.state.userEmail}`).then(response => {
-          console.log(response);
-          this.setState({
-              books: response.data[0].books,
-          })
-      }).catch(error => {
-          console.log(error.message);
-      });
+}
+
+showAddForm = (boolean) => {
+    this.setState({
+        showForm: boolean,
+    });
+}
+
+
+handleAddBook = async (e) => {
+    e.preventDefault();
+    let potato = "Not available";
+    if (e.target.status.checked) {
+        potato = 'Available';
     }
 
-  render() {
-      return (
-          <div>
-              {
-                  this.state.books.length>0 && 
-                  this.state.books.map((value,indx) => <Book key={this.state.books.name} bookData={this.state.books[indx]} />)  
-              }
-          </div>
-      )
-  }
+    let requestBody = {
+        email: this.state.userEmail,
+        book: {
+            name: e.target.bookName.value,
+            description: e.target.description.value,
+            status: 'good'
+        }
+    }
+
+    await axios.post(`${process.env.REACT_APP_URL}/book`, requestBody)
+        .then(response => {
+            console.log(response.data);
+            this.setState({
+                books: response.data.books,
+            })
+        })
+        .catch(error => alert(error.message))
+}
+
+handleDelete = async (index, event) => {
+    event.preventDefault();
+    console.log(index);
+    await axios.delete(`${process.env.REACT_APP_URL}/book/${index}?email=${this.state.userEmail}`).then(response => {
+        this.setState({
+            books: response.data.books,
+        })
+    })
+        .catch(error => alert(error.message))
+
+}
+
+handleUpdate = async (index, e) => {
+    e.preventDefault();
+    console.log(index);
+    let potato = "Not available";
+    if (e.target.status.checked) {
+        potato = 'Available';
+    }
+    let reqBody = {
+        email: this.state.userEmail,
+        book: {
+            name: e.target.bookName.value,
+            description: e.target.description.value,
+            status: potato
+        }
+
+    }
+    console.log(reqBody);
+    await axios.put(`${process.env.REACT_APP_URL}/book/${index}`, reqBody).then(response => {
+        this.setState({
+            books: response.data.books,
+        })
+    })
+        .catch(error => alert(error.message))
+
+}
+   
+
+render() {
+    return (
+        <div>
+            <Button variant="primary" size="sm" onClick={() => this.showAddForm(true)} >
+                Add Book
+            </Button>
+            <br /><br />
+
+            {
+                <Bookmodel show={this.state.showForm} onHide={() => this.showAddForm(false)} handleAddBook={this.handleAddBook} />
+            }
+
+            {
+                this.state.books.length > 0 &&
+                this.state.books.map((obj, ind) => <Book key={this.state.books[ind].name} bookData={this.state.books[ind]} index={ind} handleDelete={this.handleDelete} handleUpdate={this.handleUpdate} />)
+            }
+        </div>
+    )
+}
 }
 
   export default withAuth0(BestBooks);
